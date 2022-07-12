d4ddfe98015ea933517954fc07659bcde408dbd2
Joshua Menefee <joshua.menefee@aa.com>
2022-07-12 00:20:57 -0500

    Forgot to update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var discord = require("discord.js");
 var discord_js_1 = require("discord.js");
 var dotenv = require("dotenv");
 dotenv.config();
+var PREFIX = '$';
 var client = new discord.Client({
     intents: [
         discord_js_1.Intents.FLAGS.GUILDS,
@@ -16,10 +17,8 @@ client.on('ready', function () {
     console.log("Bot is ready");
 });
 client.on('messageCreate', function (msg) {
-    if (msg.content == 'ping') {
-        msg.reply({
-            content: "pong"
-        });
+    if (msg.content === "".concat(PREFIX, "ping")) {
+        msg.channel.send("\uD83C\uDFD3Latency is ".concat(Date.now() - msg.createdTimestamp, "ms. API Latency is ").concat(Math.round(client.ws.ping), "ms"));
     }
 });
 client.login(process.env.TOKEN);
