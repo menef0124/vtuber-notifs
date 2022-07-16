eb4d053b14c048ec67b8c721a6001a09486adce3
Joshua Menefee <joshua.menefee@aa.com>
2022-07-15 19:47:10 -0500

    Livestream polling works and sending list of users to ping works

--- index.ts
+++ index.ts
@@ -10,27 +10,27 @@ let db: any;
 (async () => {
     // open the database
     db = await open({
-      filename: './vtubers.sqlite',
-      driver: sqlite3.Database
+        filename: './vtubers.sqlite',
+        driver: sqlite3.Database
     });
 })();
 
 export type Livestream = {
-    name: string
-    roleId: string,
+    name: string,
     streamUrl: string,
-    stillLive: boolean
+    members: string,
+    stillLive: number
 };
 
 let mentionList: Livestream[] = [];
 
 const PREFIX: string = '$';
-const POLLING_TIMER: number = 600000; //milliseconds
+const POLLING_TIMER: number = 10000; //milliseconds
 
 let hiCount = 0;
 
 const client = new discord.Client({
-    intents:[
+    intents: [
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MESSAGES,
         Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
@@ -42,44 +42,48 @@ client.on('ready', () => {
     console.log("Bot is up");
     const notifsChannel = "996167357313601607";
     //const testChannel = "995890408846536796";
-    let interval = setInterval( async () => {
+    let interval = setInterval(async () => {
         let streamList = await pollStreams();
-        for(let i = 0; i < streamList.length; i++){
-            console.log(i);
-            //(client.channels.cache.get(notifsChannel) as discord.TextChannel).send(`<@&${streamList[i].roleId}> ` + `${streamList[i].name} is live!\n${streamList[i].streamUrl}`);
+        for (let i = 0; i < streamList.length; i++) {
+            let members: string[] = streamList[i].members.split(',');
+            let pings: string = "";
+            for (let j = 0; j < members.length; j++) {
+                pings += `<@&${members[j]}> `;
+            }
+            (client.channels.cache.get(notifsChannel) as discord.TextChannel).send(pings + `${streamList[i].name} is live!\n${streamList[i].streamUrl}`);
         }
     }, POLLING_TIMER);
 });
 
 client.on('messageCreate', (msg) => {
-    if(msg.channelId != "996167357313601607" && msg.channelId != "995890408846536796"){
+    if (msg.channelId != "996167357313601607" && msg.channelId != "995890408846536796") {
         return;
     }
-    else{
+    else {
         //Don't react to messages if they're from the bot
-        if(msg.author.bot){
+        if (msg.author.bot) {
             return;
         }
 
         //Silly ping command
-        if(msg.content.toLowerCase() === 'ping'){
+        if (msg.content.toLowerCase() === 'ping') {
             msg.reply("pong");
         }
 
         //Ping command
-        if(msg.content.toLowerCase() === `${PREFIX}ping`){
+        if (msg.content.toLowerCase() === `${PREFIX}ping`) {
             msg.channel.send(`🏓Latency is ${Date.now() - msg.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
         }
 
         //Silly hi if hi is sent 3 times
-        if(msg.content.toLowerCase() == 'hi'){
+        if (msg.content.toLowerCase() == 'hi') {
             hiCount++;
-            if(hiCount >= 3){
+            if (hiCount >= 3) {
                 msg.channel.send("hi :)");
                 hiCount = 0;
             }
         }
-        if(msg.content.toLowerCase() != 'hi'){
+        if (msg.content.toLowerCase() != 'hi') {
             hiCount = 0;
         }
     }
--- polling.ts
+++ polling.ts
@@ -1,21 +1,94 @@
 import { Livestream } from "./index";
 import sqlite3 from 'sqlite3';
 import { open } from 'sqlite';
+import fetch from "node-fetch";
+import { parse } from 'node-html-parser';
 
 let db: any;
 (async () => {
     // open the database
     db = await open({
-      filename: './vtubers.sqlite',
-      driver: sqlite3.Database
+        filename: './vtubers.sqlite',
+        driver: sqlite3.Database
     });
 })();
 
+const EXPECTED_START = "var ytInitialPlayerResponse = ";
+
 let streams: Livestream[] = [];
 
-export async function pollStreams(): Promise<Livestream[]>{
-    const sql = 'SELECT * FROM streams';
+export async function pollStreams(): Promise<Livestream[]> {
+    let streamsToReturn: Livestream[] = [];
+    let sql = 'SELECT * FROM streams';
     streams = await db.all(sql);
+    for (let i = 0; i < streams.length; i++) {
+        let status = streams[i].stillLive;
+        try {
+            const res = await fetch(streams[i].streamUrl);
+            const ytHtml = await res.text();
+            if (checkIfLive(ytHtml) && status == 0) {
+                console.log(`${streams[i].name} is now live!`);
+                sql = "UPDATE streams SET stillLive = ? WHERE id = ?";
+                db.run(sql, [1, i + 1])
+                streamsToReturn.push(streams[i]);
+            }
+            if (checkIfLive(ytHtml) && status == 1) {
+                console.log(`${streams[i].name} is online`);
+            }
+            if (!checkIfLive(ytHtml)) {
+                console.log(`${streams[i].name} is offline`);
+                sql = "UPDATE streams SET stillLive = ? WHERE id = ?";
+                db.run(sql, [0, i + 1]);
+            }
+        }
+        catch (e) {
+            console.log("There was an error", e);
+            continue;
+        }
+    }
+    console.log("-------------------------------------------");
+    return streamsToReturn;
+}
 
-    return streams;
+function checkIfLive(html: string): boolean {
+    const dom = parse(html, {
+        blockTextElements: {
+            script: true,
+            noscript: false,
+            style: false,
+            pre: false
+        }
+    });
+
+    const scripts = dom.querySelectorAll("script");
+    let playerInfo = null;
+    for (let i = 0; i < scripts.length; i++) {
+        const text = scripts[i].textContent;
+        if (text.startsWith(EXPECTED_START) && (playerInfo = initialPlayerResponse(scripts[i].textContent))) {
+            break;
+        }
+    }
+
+    if (!playerInfo) {
+        return false;
+    }
+
+    const videoDetails = playerInfo.videoDetails;
+    if (videoDetails?.isLiveContent && !videoDetails?.isUpcoming) {
+        return true;
+    }
+
+    return false;
+}
+
+function initialPlayerResponse(script: string): any {
+    let idxs = Array.from(script.matchAll(/;/g), (m) => m.index);
+    for (let i = idxs.length - 1; i >= 0; i--) {
+        try {
+            return JSON.parse(script.substring(EXPECTED_START.length, idxs[i]));
+        }
+        catch {
+
+        }
+    }
 }
